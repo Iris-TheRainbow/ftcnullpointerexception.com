d9de78c8a9ed73fb7f0dc75671f4b72779b3c68c
iris <snazzsinclair@gmail.com>
2024-08-08 23:57:33 -0500

    wow a lot changed idek what i did anymore

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,35 +1,31 @@
 function generateElements(){
     document.getElementById("footer").innerHTML = `
-        <div class="footerContainer">
-            <hr>
-            <div>
-                <div style="margin-top: 15px;">
-                    <div class="footerColumn">
-                        <button onclick="window.location.href='/'">Home</button>
-                    </div>
-                    <div class="footerColumn">
-                        <button onclick="window.location.href='robots'">Robots</button>
-                    </div>
-                    <div class="footerColumn">
-                        <button onclick="window.location.href='sponsors'">Sponsors</button>
-                    </div>
-                    <div class="footerColumn">
-                        <button onclick="window.location.href='outreach'">Outreach</button>
-                    </div>
-                    <div class="footerColumn">
-                        <button onclick="window.location.href='portfolios'">Portfolios</button>
-                    </div>
-                    <div class="footerColumn" style="float: right;">
-                        <button onclick="window.location.href='mailto:pblhsrobotics@gmail.com'">Email our team!</button>
-                    </div>
+            <div class="footerContainer">
+                <hr>
+                <div>
+                    <button onclick="window.location.href='robots'">Robots</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='sponsors'">Sponsors</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='outreach'">Outreach</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='portfolios'">Portfolios</button>
+                </div>
+                <div>
+                    <button onclick="window.location.href='gallery'">Gallery</button>
+                </div>
+                <div style="float: right;">
+                    <button onclick="window.location.href='mailto:pblhsrobotics@gmail.com'">Email our team!</button>
                 </div>
             </div>
-        </div>
     `
     document.getElementById("headder").innerHTML = `
         <div class="headderContainer">
-            <div class="headderColumn" style="width: fit-content;">
-                <button onclick="window.location.href='/'" style="font-size: 350%; width: fit-content; max-width: 45%; background-color: #202020;"><img src="img/boltnogears.png" style="width: 15%;"> BoltBusterZ</button>
+            <div class="headderColumn" style="width: fit-content; display: flex;">
+                <button onclick="window.location.href='/'" style="font-size: 350%; width: fit-content; max-width: 45%; background-color: #202020;"><img src="img/boltnogears.png" style="width: 15%; border-radius: 20%; background-color: azure; margin-top: .5vw;"> BoltBusterZ</button>
             </div>
         </div>`
 
