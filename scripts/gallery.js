4c375cc234dee7985e17dced83d59c6cff9cc522
iris <snazzsinclair@gmail.com>
2024-08-08 21:16:05 -0500

    action for creating the image list

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -12,5 +12,5 @@ function generateGallery(){
             console.log(string)
         })
         .catch((e) => console.error(e));
-    document.getElementById("images").innerHTML = string    
+    document.getElementById("images").innerHTML = `<img src="img/PPLeagueTourny.jpg"> <img src="img/15031.jpg"> <img src="img/BBZ15031.jpg"> <img src="img/sponsorepaxtonwarehouse.jpg"> <img src="img/CSNightBeforeLT1.jpg"> <img src="img/CSLM1.jpg"> <img src="img/boltwithgears.png"> <img src="img/CSNightBeforeLT2.jpg"> <img src="img/boltnogears.png"> <img src="img/CSLeagueTourny.jpg"> <img src="img/CSLM3.jpg"> <img src="img/BBZRobotics.png"> <img src="img/pbl.jpg"> `   
 }
