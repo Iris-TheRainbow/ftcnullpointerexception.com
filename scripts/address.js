d41599c2cf86df0a745a65bb0491d6edf293343e
iris <snazzsinclair@gmail.com>
2024-10-17 10:56:19 -0500

    does that not work twice?

--- scripts/address.js
+++ scripts/address.js
@@ -1,4 +1,5 @@
 function getAddress(){
     document.getElementById("address").innerHTML = window.location.href;
+    document.getElementById("address2").innerHTML = window.location.href;
     document.getElementById("extention").innerHTML = window.location.pathname;
 }
