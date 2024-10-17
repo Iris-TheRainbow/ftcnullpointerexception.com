b953934d8f71eccc2b8773a2d90d3e736a123f5b
iris <snazzsinclair@gmail.com>
2024-10-17 13:38:49 -0500

    add 404 links

--- scripts/address.js
+++ scripts/address.js
@@ -1,5 +1,5 @@
 function getAddress(){
-    document.getElementById("address").innerHTML = window.location.href;
+    document.getElementById("address").innerHTML = "<a href=\"" + window.location.href + "\">" + window.location.href + "</a>";
     document.getElementById("address2").innerHTML = window.location.href;
     document.getElementById("extention").innerHTML = window.location.pathname;
 }
